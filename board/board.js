8126de05f83f44a45ba58d862401cdce1acba47e
jong_min <whdals0525@gmail.com>
2024-03-02 21:08:35 +0900

    first commit

---
+++ board/board.js
@@ -0,0 +1,14 @@
+let navOpen = false; // 네비게이션 상태를 저장하는 변수
+
+const toggleNav = () => {
+  const nav = document.getElementById("mySideNav");
+  if (!navOpen) {
+    // 네비게이션이 닫혀 있는 경우
+    nav.style.width = "270px"; // 네비게이션 열기
+    navOpen = true;
+  } else {
+    // 네비게이션이 열려 있는 경우
+    nav.style.width = "0px"; // 네비게이션 닫기
+    navOpen = false;
+  }
+};
