7eaa0b7083a3ea30066c5a517d9e41645fb1727c
SeungJun31 <159690971+SeungJun31@users.noreply.github.com>
2024-03-03 16:31:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,11 @@
 let puppyList = [];
 let pop = [];
 const apiKey = `50a90492567241f98925e8b285acfa3f`;
-const dog = document.getElementById("dog-icon");
-const cat = document.getElementById("cat-icon");
 const species = document.getElementById("button-species");
 const local = document.getElementById("button-local");
 let url = new URL(
   `https://openapi.gg.go.kr/AbdmAnimalProtect?type=json&pIndex=4&pSize=150&key=${apiKey}`
 );
-dog.addEventListener("click", () => getListDog(event));
-cat.addEventListener("click", () => getListCat(event));
 species.addEventListener("click", () => getListBySpecies());
 local.addEventListener("click", () => getListByLocal());
 
@@ -68,59 +64,59 @@ const render = () => {
         "[개]",
         "[강아지]"
       )} | ${(() => {
-          if (parseInt(item.AGE_INFO) == 2024) {
-            return "1살 미만";
-          } else if (parseInt(item.AGE_INFO) == 2023) {
-            return "1살";
-          } else if (parseInt(item.AGE_INFO) == 2022) {
-            return "2살";
-          } else if (parseInt(item.AGE_INFO) == 2021) {
-            return "3살";
-          } else if (parseInt(item.AGE_INFO) == 2020) {
-            return "4살";
-          } else if (parseInt(item.AGE_INFO) == 2019) {
-            return "5살";
-          } else if (parseInt(item.AGE_INFO) == 2018) {
-            return "6살";
-          } else if (parseInt(item.AGE_INFO) == 2017) {
-            return "7살";
-          } else if (parseInt(item.AGE_INFO) == 2016) {
-            return "8살";
-          } else if (parseInt(item.AGE_INFO) == 2015) {
-            return "9살";
-          } else if (parseInt(item.AGE_INFO) == 2014) {
-            return "10살";
-          } else if (parseInt(item.AGE_INFO) == 2013) {
-            return "11살";
-          } else if (parseInt(item.AGE_INFO) == 2012) {
-            return "12살";
-          } else if (parseInt(item.AGE_INFO) == 2011) {
-            return "13살";
-          } else if (parseInt(item.AGE_INFO) == 2010) {
-            return "14살";
-          } else if (parseInt(item.AGE_INFO) == 2009) {
-            return "15살";
-          } else if (parseInt(item.AGE_INFO) == 2008) {
-            return "16살";
-          } else if (parseInt(item.AGE_INFO) == 2007) {
-            return "17살";
-          } else if (parseInt(item.AGE_INFO) == 2006) {
-            return "18살";
-          }
-        })()}</div>
+        if (parseInt(item.AGE_INFO) == 2024) {
+          return "1살 미만";
+        } else if (parseInt(item.AGE_INFO) == 2023) {
+          return "1살";
+        } else if (parseInt(item.AGE_INFO) == 2022) {
+          return "2살";
+        } else if (parseInt(item.AGE_INFO) == 2021) {
+          return "3살";
+        } else if (parseInt(item.AGE_INFO) == 2020) {
+          return "4살";
+        } else if (parseInt(item.AGE_INFO) == 2019) {
+          return "5살";
+        } else if (parseInt(item.AGE_INFO) == 2018) {
+          return "6살";
+        } else if (parseInt(item.AGE_INFO) == 2017) {
+          return "7살";
+        } else if (parseInt(item.AGE_INFO) == 2016) {
+          return "8살";
+        } else if (parseInt(item.AGE_INFO) == 2015) {
+          return "9살";
+        } else if (parseInt(item.AGE_INFO) == 2014) {
+          return "10살";
+        } else if (parseInt(item.AGE_INFO) == 2013) {
+          return "11살";
+        } else if (parseInt(item.AGE_INFO) == 2012) {
+          return "12살";
+        } else if (parseInt(item.AGE_INFO) == 2011) {
+          return "13살";
+        } else if (parseInt(item.AGE_INFO) == 2010) {
+          return "14살";
+        } else if (parseInt(item.AGE_INFO) == 2009) {
+          return "15살";
+        } else if (parseInt(item.AGE_INFO) == 2008) {
+          return "16살";
+        } else if (parseInt(item.AGE_INFO) == 2007) {
+          return "17살";
+        } else if (parseInt(item.AGE_INFO) == 2006) {
+          return "18살";
+        }
+      })()}</div>
       <div id="gender" class="gender">${
         item.SEX_NM == "M"
           ? item.SEX_NM.replace("M", "남아")
           : item.SEX_NM.replace("F", "여아")
       }  · ${(() => {
-          if (item.NEUT_YN == "Y") {
-            return "중성화 완료";
-          } else if (item.NEUT_YN == "N") {
-            return "중성화 미완료";
-          } else {
-            return "중성화 알수없음";
-          }
-        })()}</div>
+        if (item.NEUT_YN == "Y") {
+          return "중성화 완료";
+        } else if (item.NEUT_YN == "N") {
+          return "중성화 미완료";
+        } else {
+          return "중성화 알수없음";
+        }
+      })()}</div>
     </div>
     <div class="address-box">
       <span>지역: ${item.JURISD_INST_NM}</span>
